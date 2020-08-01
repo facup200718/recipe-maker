640078a6e3f134c6c9d8be3ee685b2c4dd8fc772
Facundo Perez <facup200718@gmail.com>
2020-08-01 04:47:55 -0300

    Mostrando el valor en caso de que sea null

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,13 @@
 import java.lang.NullPointerException
 
 fun main(args: Array<String>) {
+
+    var palabra: String? = null
+    var largo: Int? = palabra?.length
+    println("El largo de la palabra es de: $largo")
+
     try {
-        var compute: String?
-        compute = null
+        var compute: String? = null
         var longitud: Int = compute!!.length
     } catch (e: NullPointerException) {
         println("Ingresa un valor, no aceptamos valores nulos")
